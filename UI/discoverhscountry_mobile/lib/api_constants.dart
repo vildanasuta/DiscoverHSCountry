ff846bded9852922d8b7794a5c6b36a6970b82a6
Vildana Suta <vildana.suta@hpe.com>
2023-12-25 11:50:35 +0100

    Edited to use Platform.environment

--- UI/discoverhscountry_mobile/lib/api_constants.dart
+++ UI/discoverhscountry_mobile/lib/api_constants.dart
@@ -1,13 +1,15 @@
 // api_constants.dart
 
+import 'dart:io';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static final String baseUrl = _getBaseUrl();
 
   static String _getBaseUrl() {
-    final String host = dotenv.env['API_HOST']??'localhost';
-    final String port = dotenv.env['API_PORT']??'7125';
+    final String host = Platform.environment['API_HOST']??'localhost';
+    final String port = Platform.environment['API_PORT']??'7125';
     return 'http://$host:$port';
   }
 }
