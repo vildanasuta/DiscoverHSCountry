da1e079880b8e228c2bd1e3f2594159329ce068d
Vildana Suta <vildana.suta@hpe.com>
2024-01-29 07:20:42 +0100

    Edited usage of env variables

--- UI/discoverhscountry_desktop/lib/api_constants.dart
+++ UI/discoverhscountry_desktop/lib/api_constants.dart
@@ -6,8 +6,8 @@ class ApiConstants {
   static final String baseUrl = _getBaseUrl();
 
   static String _getBaseUrl() {
-    final String host = Platform.environment['API_HOST'] ?? 'localhost';
-    final String port = Platform.environment['API_PORT'] ?? '7125';
+    const String host = String.fromEnvironment('API_HOST', defaultValue: 'localhost');
+    const String port = String.fromEnvironment('API_PORT', defaultValue: '7125');
 
     return 'http://$host:$port';
   }
